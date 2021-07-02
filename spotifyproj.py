fe774cf3d0148c249943631717841d2a12309fc5
Tersis Wolle <wolle@susqu.edu>
2021-07-02 19:27:43 +0000

    checkstyle updates

--- spotifyproj.py
+++ spotifyproj.py
@@ -1,38 +1,25 @@
 import spotipy
 import requests
-CLIENT_ID="b17aa41ea56f4d8e931b210c3a766e70"
-CLIENT_SECRET="18563009e3fc439fa5f0bc18224f57a6"
-
-AUTH_URL='https://accounts.spotify.com/api/token'
-auth_response= requests.post(AUTH_URL,{
+CLIENT_ID = "b17aa41ea56f4d8e931b210c3a766e70"
+CLIENT_SECRET = "18563009e3fc439fa5f0bc18224f57a6"
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+auth_response = requests.post(AUTH_URL, {
   'client_id':CLIENT_ID,
   'grant_type':'client_credentials',
   'client_secret': CLIENT_SECRET
 })
-
-#check if status code is 200
+# check if status code is 200
 print(auth_response.status_code)
 auth_response_data=auth_response.json()
 
 access_token=auth_response_data['access_token']
-#recaching, but check if auth_response_data is not null
+# reaching, but check if auth_response_data is not null
 print(auth_response_data)
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
-
 BASE_URL='https://api.spotify.com/v1/'
 track_id = '1ivUwqx8QXtteFsxAOF5X1'
 r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
 r = r.json()
 print(r)
-
-
-
-  
-  
- 
-
-  
-
-
