cc1f4445ca3ececffd002cef46e5bb20530322b4
Tersis Wolle <wolle@susqu.edu>
2021-07-05 02:56:54 +0000

    loaded to database

--- spotifyproj.py
+++ spotifyproj.py
@@ -1,5 +1,8 @@
 import spotipy
 import requests
+import sqlalchemy
+from sqlalchemy import create_engine
+import pandas
 
 
 CLIENT_ID = "b17aa41ea56f4d8e931b210c3a766e70"
@@ -11,13 +14,13 @@ auth_response = requests.post(AUTH_URL, {
   'client_secret': CLIENT_SECRET
 })
 # check if status code is 200
-print(auth_response.status_code)
+# print(auth_response.status_code)
 auth_response_data = auth_response.json()
 
 
 access_token = auth_response_data['access_token']
 # reaching, but check if auth_response_data is not null
-print(auth_response_data)
+# print(auth_response_data)
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
@@ -25,4 +28,12 @@ BASE_URL = 'https://api.spotify.com/v1/'
 track_id = '1ivUwqx8QXtteFsxAOF5X1'
 r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
 r = r.json()
-print(r)
+df = pandas.DataFrame(list(r.items()), columns = ['Feature', 'Score'])
+
+
+engine = create_engine('mysql://root:codio@localhost/Spotify')
+
+df.to_sql('Audio Features', con = engine, if_exists = 'replace', index = False)
+
+# print(r)
+# print(df)
